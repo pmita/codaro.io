61505acc77ff631db3c127c2a4dbc7b5a97bfea7
pmita <panos.mita2@outlook.com>
2023-03-16 21:58:02 +0000

    improving on the navbar

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -1,15 +1,30 @@
 'use client'
 
 import { roboto, poppins } from '../utils/fonts'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className="mx-8 md:mx-6 md:xl-4 my-4 flex justify-between items-center">
-      <h1 className={`text-2xl ${roboto.className} text-main-white bg-main-black p-4`}>
-        Codaro<span className="text-highlight-green">.io</span>
+    <nav className="px-8 md:px-6 md:xl-4 p-4 flex justify-between items-center bg-main-black text-main-white">
+      <h1 className={`text-2xl ${poppins.className} bg-highlight-green p-4`}>
+        Codaro<span className="text-secondary-black">.io</span>
       </h1>
-      <ul>
-        <h4>Test</h4>
+      <ul className="flex justify-end items-center gap-x-4">
+        <h4 className="border-solid border-4 border-highlight-green p-2 rounded-md">
+          <Link href={'/pro'}>
+            PRO
+          </Link>
+        </h4>
+        <h4>
+          <Link href={'/courses'}>
+            courses
+          </Link>
+        </h4>
+        <button>
+          <Link href={'/login'} className="p-4 bg-main-white text-main-black">
+            LOGIN
+          </Link>
+        </button>
       </ul>
     </nav>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Welcome to Next.js</h1>
-      <h4 className="text-gray-500 py-8 bg-main-black">With Tailwind CSS</h4>
     </main>
   )
 }
