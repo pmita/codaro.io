ad2d8174705ff62560f54a4dfecdb4530c046878
pmita <panos.mita2@outlook.com>
2023-03-21 23:13:46 +0000

    creating initial animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,25 @@ module.exports = {
         poppins: ["var(--font-poppins)"],
       },
     },
+    keyframes: {
+      down: {
+        '0%, 100%': { transform: 'translateY(-10%)' },
+        '50%': { transform: 'translateY(10%)' },
+      },
+      up: {
+        '0%, 100%': { transform: 'translateY(10%)' },
+        '50%': { transform: 'translateY(-10%)' },
+      },
+      right: {
+        '0%, 100%': { transform: 'rotateZ(-10deg)' },
+        '50%': { transform: 'rotateZ(15deg)' },
+      },
+    },
+    animation: {
+      floatDown: 'down 7s ease-in-out infinite',
+      floatUp: 'up 5s ease-in-out infinite',
+      rotateRight: 'right 9s ease-in-out infinite',
+    },
   },
   plugins: [],
   darkMode: "class",
