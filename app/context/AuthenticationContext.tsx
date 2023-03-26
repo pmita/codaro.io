6b4a76e4b00570d0ff415dbcc18864c229e875ae
pmita <panos.mita2@outlook.com>
2023-03-26 22:45:06 +0100

    useReducer introduced

--- app/context/AuthenticationContext.tsx
+++ app/context/AuthenticationContext.tsx
@@ -3,18 +3,47 @@
 import { createContext, useContext, useReducer } from "react"
 
 export type AuthStateContextType = {
-  color?: string
+  state: AuthStateType,
+  dispatch: React.Dispatch<any>
 }
 
 export type AuthStateProviderType = {
   children: React.ReactNode
 }
 
+export type AuthStateType = {
+  color: string,
+  authStateHasChanged: boolean,
+  user: any,
+  isLoading: boolean,
+  error: Error | string | null,
+}
+
+const initialState: AuthStateType = {
+  color: 'red',
+  authStateHasChanged: false,
+  user: null,
+  isLoading: false,
+  error: null,
+}
+
 export const AuthStateContext = createContext<AuthStateContextType | null>(null);
 
+export const AuthStateReducer = (state: AuthStateType, action: any) => {
+  switch(action.type) {
+    case 'TEST_ONE':
+    case 'TEST_TWO':
+      return { ...state };
+    default:
+      return { ...state };
+  }
+}
+
 export const AuthStateProvider = ({ children }: AuthStateProviderType) => {
+  // STATE
+  const [state, dispatch] = useReducer(AuthStateReducer, initialState);
   return (
-    <AuthStateContext.Provider value={{ color: 'blue' }}>
+    <AuthStateContext.Provider value={{ state, dispatch }}>
       {children}
     </AuthStateContext.Provider>
   );
