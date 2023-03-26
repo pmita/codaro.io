210751b6a0f8923d1115f0e1093681fcdae13c51
pmita <panos.mita2@outlook.com>
2023-03-26 23:18:20 +0100

    fixing typescript

--- app/context/AuthenticationContext.tsx
+++ app/context/AuthenticationContext.tsx
@@ -2,10 +2,17 @@
 
 import { createContext, useContext, useReducer } from "react"
 // TYPES
-import { AuthStateType, AuthStateContextType, AuthStateProviderType } from "../types/context/AuthStateContextTypes";
+import { 
+  AuthStateType, 
+  AuthStateContextType, 
+  AuthStateProviderType, 
+  AuthActionType, 
+  SignInSuccessActionType, 
+  SignInLoadingActionType, 
+  SignInErrorActionType 
+} from "../types/context/AuthStateContextTypes";
 
 const initialState: AuthStateType = {
-  color: 'red',
   authStateHasChanged: false,
   user: null,
   isLoading: false,
@@ -14,11 +21,17 @@ const initialState: AuthStateType = {
 
 export const AuthStateContext = createContext<AuthStateContextType | null>(null);
 
-export const AuthStateReducer = (state: AuthStateType, action: any) => {
+export const AuthStateReducer = (
+  state: AuthStateType, 
+  action: SignInSuccessActionType | SignInLoadingActionType | SignInErrorActionType
+) => {
   switch(action.type) {
-    case 'TEST_ONE':
-    case 'TEST_TWO':
-      return { ...state };
+    case AuthActionType.SIGN_IN_LOADING:
+      return { ...state, isLoading: true, user: null, error: null };
+    case AuthActionType.SIGN_IN_ERROR:
+      return { ...state, isLoading: false, user: null, error: action.payload };
+    case AuthActionType.SIGN_IN_SUCCESS:
+      return { ...state, isLoading: false, user: action.payload, error: null };
     default:
       return { ...state };
   }
--- app/types/context/AuthStateContextTypes.ts
+++ app/types/context/AuthStateContextTypes.ts
@@ -1,9 +1,28 @@
 export type AuthStateType = {
-  color: string,
   authStateHasChanged: boolean,
   user: any,
   isLoading: boolean,
   error: Error | string | null,
+}
+
+// -----Action Reducer Types-----
+export enum AuthActionType {
+  SIGN_IN_LOADING="SIGN_IN_LOADING",
+  SIGN_IN_SUCCESS="SIGN_IN_SUCCESS",
+  SIGN_IN_ERROR="SIGN_IN_ERROR"
+}
+
+export type SignInSuccessActionType = {
+  type: AuthActionType.SIGN_IN_SUCCESS,
+  payload: any
+}
+export type SignInLoadingActionType = {
+  type: AuthActionType.SIGN_IN_LOADING,
+}
+
+export type SignInErrorActionType = {
+  type: AuthActionType.SIGN_IN_ERROR,
+  payload: Error | string | null
 }
 
 // -----Context Types-----
