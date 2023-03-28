7904a2b1c93667918f2db4cfb9d0f95ca8afbcaa
pmita <panos.mita2@outlook.com>
2023-03-28 21:29:35 +0100

    useSignUp hook added

---
+++ app/hooks/useSignUp.ts
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useAuthState } from '../context/AuthenticationContext';
+import { auth, firestore } from '../firebase/config';
+
+type signUp = (email: string, password: string, username: string) => void;
+
+export const useSignUp = () => {
+  const { dispatch } = useAuthState();
+  const [error, setError] = useState<Error | string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signUp: signUp = async (email, password, username) => {
+    setIsLoading(true);
+
+    try{
+      setIsLoading(true);
+      setError(null);
+      const response = await auth.signInWithEmailAndPassword(email, password);
+
+      if(!response.user){
+        throw new Error('Sign up process failed. User could not be created.');
+      }
+
+      await response.user.updateProfile({
+        displayName: username
+      })
+
+      await firestore.collection('users').doc(response.user.uid).set({
+        displayName: username,
+        email,
+        uid: response.user.uid,
+      })
+
+      dispatch({ type: 'SIGN_UP', payload: response.user });
+    }catch(error){
+      setIsLoading(false);
+      setError((error as Error).message);
+    }
+  }
+
+  return { signUp, error, isLoading };
+}
