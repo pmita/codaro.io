04d08b74648ca6202aeae61294f7d8b6365c8f3f
pmita <panos.mita2@outlook.com>
2024-12-12 00:06:18 +0000

    chore: guarding dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
+import { AuthCheck } from "@/components/auth-check";
 import { SignoutButton } from "@/components/buttons/signout-button";
 
 export default async function DashboardPage(){
   return (
-    <main className="min-h-[90dvh] w-full flex justify-center items-stretch">
-      <SignoutButton />
+    <main className="min-h-[90dvh] grid place-items-center">
+      <AuthCheck fallback={(<h1>You need to be signed in</h1>)}>
+        <SignoutButton />
+      </AuthCheck>
     </main>
   )
 }
