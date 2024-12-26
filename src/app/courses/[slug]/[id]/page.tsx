6bfcbf27ba01dfb373060701698c8a788c0e6830
pmita <panos.mita2@outlook.com>
2024-12-26 19:19:38 +0000

    style: introducing the dynamic chapter page layout

--- src/app/courses/[slug]/[id]/page.tsx
+++ src/app/courses/[slug]/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { getCourseChapter } from '@/data/content/course';
-import { MdxLayout } from "@/components/layouts/content/course";
+import { MdxLayout, PageLayout } from "@/components/layouts/content/course";
 import { Mdx } from '@/components/mdx';
 
 interface ChapterPageProps {
@@ -14,10 +14,20 @@ export default async function ChapterPage(props: ChapterPageProps) {
   const data = await getCourseChapter(course, chapter);
 
   return (
-    // <div className="p-4">
+    <PageLayout>
+      <div className="grid place-items-center w-full h-[650px] bg-primary">
+        <h1>Video Player goes here</h1>
+      </div>
+      <div className="grid place-items-center w-full h-[100px] bg-secondary">
+        <h1>Controls go here</h1>
+      </div>
+      <div className="flex flex-col justify-center items-start gap-5">
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
+      </div>
       <MdxLayout>
         <Mdx mdxSource={data.mdx } />
       </MdxLayout>
-    // </div> 
+    </PageLayout>
   );
 }
