90f95089d29518c2a1463453bc0766738a3f71ca
pmita <panos.mita2@outlook.com>
2025-04-18 00:01:01 +0100

    feat: adding stripe portal server actions

---
+++ src/data/stripe/portal.ts
@@ -0,0 +1,42 @@
+"use server"
+
+// DATA
+import { getCurrentUser } from "@/data/auth/currentUser";
+import { getOrCreateStripeCustomer } from '@/data/db/customer';
+// LIB
+import { stripe } from "@/lib/stripe/server/config";
+
+const PORTAL_RETURN_URL = "http://localhost:3000/dashboard"
+
+export const initStripePortal = async () => {
+    try {
+      const user = await getCurrentUser();
+      
+      if (!user) {
+        throw new Error("Unauthorized");
+      }
+
+      const customer = await getOrCreateStripeCustomer(user.uid);
+
+      const session = await createStripePortalSession(customer.id);
+
+      if (session) {
+        return session;
+      }
+    } catch(error) {
+      console.error("Error creating checkout session:", error);
+      return { error: 'Could not create checkout session successfully' };
+    }
+  }
+
+  export const createStripePortalSession = async (customerId: string) => {
+    const session = await stripe.billingPortal.sessions.create({
+      customer: customerId,
+      return_url: PORTAL_RETURN_URL,
+    })
+
+    return {
+      url: session.url,
+      id: session.id
+    }
+  }
