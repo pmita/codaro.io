980a5bef6bc8ff1f9aae8689a3d27298b05c013b
pmita <panos.mita2@outlook.com>
2025-04-21 17:21:23 +0100

    chore: updating db tier types

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,8 +1,22 @@
 export enum PRO_STATUS {
-  LIFE_TIME = 'lifetime',
   ACTIVE = 'active',
   PAST_DUE = 'past_due',
   CANCELED = 'canceled',
   UNPAID = 'unpaid',
-  FREE = 'free'
+  FREE = 'free',
+  BASIC = 'basic',
+  PRO = 'pro',
+  LIFE_TIME = 'lifetime',
 }
+
+/*
+  Stripe subscription status include the following values:
+  - incomplete
+  - incomplete_expired
+  - trialing <- We don't use this
+  - active
+  - past_due
+  - canceled
+  - unpaid
+  - paused
+*/
