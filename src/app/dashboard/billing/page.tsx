cd065af21f1f19b8c97df58e88c2b4cf4630dba9
pmita <panos.mita2@outlook.com>
2025-04-30 13:06:17 +0100

    chore: wrapping billing component in suspense and handle loading state

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -2,6 +2,8 @@
 import type { Metadata } from 'next'
 // COMPONENTS
 import { UserBilling } from '@/components/cards/user-billing/UserBilling'
+import { Suspense } from 'react'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export const metadata: Metadata = {
   title: 'Billing',
@@ -10,9 +12,11 @@ export const metadata: Metadata = {
 
 export default async function BillingPage(){
   return (
-    <>
+    <main className="flex flex-col items-stretch justify-start gap-4">
       <h1>Welcome to Billing page</h1>
-      <UserBilling />
-    </>
+      <Suspense fallback={( <Skeleton className="h-96 w-full" /> )}>
+        <UserBilling />
+      </Suspense>
+    </main>
   )
 }
--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils';
 
 export const Skeleton = ({ className, ...props}: React.HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className={cn("animate-pulse rounded-md bg-primary", className)}
+    <div className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     /> 
   )
